f8b819b4af4a1ad4d4060e7a9a5b905d564f607e
Frederick Javalera <frederickjavalera@gmail.com>
2017-11-26 17:01:40 -0700

    Commit

--- src/gui/DataRetrieval.java
+++ src/gui/DataRetrieval.java
@@ -6,183 +6,205 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Calendar;
 
-public class DataRetrieval {
+public class DataRetrieval
+{
 
-    int ID= 0;
+	int ID = 0;
 
-    private Connection establishConnection(){
-        //String url = "jdbc:mysql://raspy.ciopnus8w2eh.us-west-1.rds.amazonaws.com:3306/";
-        String url = "jdbc:mysql://raspystudent.ciopnus8w2eh.us-west-1.rds.amazonaws.com:3306/";
+	private Connection establishConnection()
+	{
+		// String url =
+		// "jdbc:mysql://raspy.ciopnus8w2eh.us-west-1.rds.amazonaws.com:3306/";
+		String url = "jdbc:mysql://raspystudent.ciopnus8w2eh.us-west-1.rds.amazonaws.com:3306/";
 
-        //;sendStringParametersAsUnicode=false"
-        String userName = "raspystudent";
-        String password = "weatherstation";
-        String dbName = "raspystudent";
-        String driver = "com.mysql.jdbc.Driver";
-        Connection conn = null;
-        Statement stmt = null;
+		// ;sendStringParametersAsUnicode=false"
+		String userName = "raspystudent";
+		String password = "weatherstation";
+		String dbName = "raspystudent";
+		String driver = "com.mysql.jdbc.Driver";
+		Connection conn = null;
+		Statement stmt = null;
 
-        try{
+		try
+		{
 
-            conn = DriverManager.getConnection(url + dbName, userName, password);
-            System.out.println("Database Connection Successful!");
-        }
-        catch(Exception e){
-            System.out.println(e);
-            System.out.println("Oops something went wrong when trying to connect to the database");
+			conn = DriverManager.getConnection(url + dbName, userName, password);
+			System.out.println("Database Connection Successful!");
+		} catch (Exception e)
+		{
+			System.out.println(e);
+			System.out.println("Oops something went wrong when trying to connect to the database");
 
-        }
-        return conn;
-    }
+		}
+		return conn;
+	}
 
+	private void updateDB(Connection conn, java.sql.Date inputDate, java.sql.Time inputTime, double temperature,
+			double rainInches, double windSpeed, int humidityPercent, int ID)
+	{
 
-    private void updateDB(Connection conn, java.sql.Date inputDate, java.sql.Time inputTime,
-                          double temperature, double rainInches, double windSpeed, int humidityPercent,int ID) {
+		Statement stmt = null;
 
-        Statement stmt = null;
+		try
+		{
+			stmt = conn.createStatement();
 
-        try {
-            stmt = conn.createStatement();
+			// String sql = "insert into raspystudent.WEATHER(Date, Time, Temp, Rainfall,
+			// Wind,Humidity) " +
+			// "values(date,time,Temp,Rainfall,Wind,Humidity)";
 
-//            String sql = "insert into raspystudent.WEATHER(Date, Time, Temp, Rainfall, Wind,Humidity) " +
-//                         "values(date,time,Temp,Rainfall,Wind,Humidity)";
+			PreparedStatement pst = conn.prepareStatement("insert into WEATHER("
+					+ "Date, Time, Temperature, rainInches, windSpeed,humidityPercent,ID) values(?,?,?,?,?,?,?)");
+			pst.setDate(1, inputDate);
+			pst.setTime(2, inputTime);
+			pst.setDouble(3, temperature);
+			pst.setDouble(4, rainInches);
+			pst.setDouble(5, windSpeed);
+			pst.setDouble(6, humidityPercent);
+			pst.setInt(7, ID);
+			pst.execute();
+			// pst.close();
 
-            PreparedStatement pst = conn.prepareStatement("insert into WEATHER(" +
-                    "Date, Time, Temperature, rainInches, windSpeed,humidityPercent,ID) values(?,?,?,?,?,?,?)");
-            pst.setDate(1, inputDate);
-            pst.setTime(2, inputTime);
-            pst.setDouble(3, temperature);
-            pst.setDouble(4, rainInches);
-            pst.setDouble(5, windSpeed);
-            pst.setDouble(6, humidityPercent);
-            pst.setInt(7, ID);
-            pst.execute();
-            //pst.close();
+			// stmt.executeUpdate(sql);
+		}
 
-            //stmt.executeUpdate(sql);
-        }
+		catch (SQLException e)
+		{
+			System.out.println("SQL Exception: " + e);
+		}
+	}
 
-        catch (SQLException e) {
-            System.out.println("SQL Exception: " + e);
-        }
-    }
+	public void closeConnection(Connection conn, Statement stmt)
+	{
 
-    public void closeConnection(Connection conn, Statement stmt){
+		try
+		{
+			if (stmt != null)
+			{
+				conn.close();
+			}
+		} catch (SQLException e)
+		{
+			System.out.println("SQL Exception: " + e);
+			//
+		}
 
-            try{
-                if(stmt!=null){
-                    conn.close();
-                }
-            }
-            catch(SQLException e){
-                System.out.println("SQL Exception: " + e);
-                //
-            }
+		try
+		{
+			if (conn != null)
+			{
+				conn.close();
+			}
+		} catch (SQLException e)
+		{
+			System.out.println("SQL Exception: " + e);
+			//
+		}
+	}
 
-            try{
-                if(conn!= null){
-                    conn.close();
-                }
-            }
-            catch(SQLException e){
-                System.out.println("SQL Exception: " + e);
-                //
-            }
-    }
+	// Get all data
+	public static ResultSet getWeather(Connection conn) throws Exception
+	{
+		return getResultSet(conn, "SELECT * FROM WEATHER");
 
-    // Get all data
-    public static ResultSet getWeather(Connection conn)throws Exception {
-        return getResultSet(conn,"SELECT * FROM WEATHER");
+	}
 
-    }
+	public static ResultSet getResultSet(Connection conn, String sql) throws Exception
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Statement stmt = conn.createStatement(ResultSet.CONCUR_READ_ONLY, ResultSet.TYPE_FORWARD_ONLY);
+		return stmt.executeQuery(sql);
 
-    public static ResultSet getResultSet(Connection conn, String sql)throws Exception{
-        Class.forName("com.mysql.jdbc.Driver");
-        Statement stmt = conn.createStatement(ResultSet.CONCUR_READ_ONLY, ResultSet.TYPE_FORWARD_ONLY);
-        return stmt.executeQuery(sql);
+	}
 
-    }
+	private static Date stringToSQLDate(String string)
+	{
+		SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+		java.sql.Date sqlDate = null;
 
-    private static Date stringToSQLDate(String string){
-        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
-        java.sql.Date sqlDate = null;
+		try
+		{
+			java.util.Date date = format.parse(string);
+			sqlDate = new Date(date.getTime());
 
-        try{
-            java.util.Date date = format.parse(string);
-            sqlDate = new Date(date.getTime());
+		} catch (Exception e)
+		{
+			System.out.println("Error parsing date string: " + e);
+		}
+		return sqlDate;
+	}
 
-        }
-        catch(Exception e){
-            System.out.println("Error parsing date string: " + e);
-        }
-        return sqlDate;
-    }
+	private static Time stringToSQLTime(String string)
+	{
+		java.sql.Time sqlTime = null;
 
-    private static Time stringToSQLTime(String string){
-        java.sql.Time sqlTime = null;
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
 
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
+		try
+		{
+			java.util.Date time = simpleDateFormat.parse(string);
+			sqlTime = new Time(time.getTime());
+		} catch (Exception e)
+		{
+			System.out.println("Error parsing time string: " + e);
+		}
 
-        try{
-            java.util.Date time = simpleDateFormat.parse(string);
-            sqlTime = new Time(time.getTime());
-        }
-        catch(Exception e){
-            System.out.println("Error parsing time string: " + e);
-        }
+		return sqlTime;
+	}
 
-        return sqlTime;
-    }
+	private void deleteAll(Connection conn)
+	{
+		Statement stmt = null;
 
-    private void deleteAll(Connection conn){
-        Statement stmt = null;
+		try
+		{
+			stmt = conn.createStatement();
+			PreparedStatement pst = conn.prepareStatement("TRUNCATE WEATHER");
+			pst.execute();
 
-        try {
-            stmt = conn.createStatement();
-            PreparedStatement pst = conn.prepareStatement("TRUNCATE WEATHER");
-            pst.execute();
+		} catch (SQLException e)
+		{
 
+		}
+	}
 
-        }
-        catch(SQLException e){
+	// TODO - Set up so main method accepts args for each line as args
+	public static void main(String[] args)
+	{
 
-        }
-    }
+		long startTime = System.currentTimeMillis();
 
-    // TODO - Set up so main method accepts args for each line as args
-    public static void main(String[] args){
+		// Unit tests
+		DataRetrieval bob = new DataRetrieval();
+		Connection connection = bob.establishConnection();
 
-        long startTime = System.currentTimeMillis();
+		bob.deleteAll(connection);
 
-        // Unit tests
-        DataRetrieval bob = new DataRetrieval();
-        Connection connection = bob.establishConnection();
+		java.sql.Date myDate = stringToSQLDate("01-01-2011");
+		java.sql.Time myTime = stringToSQLTime("07:34:33");
 
-        bob.deleteAll(connection);
+		bob.updateDB(connection, myDate, myTime, 4, 3, 2, 1, 50);
 
-        java.sql.Date myDate = stringToSQLDate("01-01-2011");
-        java.sql.Time myTime = stringToSQLTime("07:34:33");
+		ResultSet rs = null;
 
-        bob.updateDB(connection,myDate,myTime,4,3,2,1,50);
+		// TODO - Turn me into a method
+		try
+		{
+			rs = getWeather(connection);
+			while (rs.next())
+			{
+				System.out.println("Date: " + rs.getString(1) + " \nTime: " + rs.getString(2) + " \nTemp: "
+						+ rs.getString(3) + " \nRain: " + rs.getFloat(4) + " \nWind: " + rs.getString(5)
+						+ " \nHumidity: " + rs.getString(6));
+			}
+		} catch (Exception e)
+		{
+			System.out.println(e);
+		}
 
-        ResultSet rs = null;
+		long endTime = System.currentTimeMillis();
+		float elapse = ((endTime - startTime) / 1000);
+		System.out.println("That took like " + elapse + " seconds");
 
-        // TODO - Turn me into a method
-        try{
-            rs = getWeather(connection);
-            while(rs.next()){
-                System.out.println("Date: " + rs.getString(1) + " \nTime: " + rs.getString(2) + " \nTemp: "
-                        + rs.getString(3) + " \nRain: " + rs.getFloat(4) + " \nWind: " +
-                        rs.getString(5) + " \nHumidity: " + rs.getString(6));
-            }
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-
-    long endTime = System.currentTimeMillis();
-    float elapse = ((endTime-startTime)/1000);
-        System.out.println("That took like " + elapse + " seconds");
-
-    }
+	}
 }
